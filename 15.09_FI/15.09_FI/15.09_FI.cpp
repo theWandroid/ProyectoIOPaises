c3834b4b86bd339f755f77150e12fd4477796813
theWandroid <68104028+theWandroid@users.noreply.github.com>
2023-10-25 15:36:34 +0200

    commit_countries

---
+++ 15.09_FI/15.09_FI/15.09_FI.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace My1509FI
+{
+	TEST_CLASS(My1509FI)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
